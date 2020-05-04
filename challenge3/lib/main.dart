ce91d3729e1d80ab85e586163c9d4cebae9f9ca5
eswar2001 <teswar2001@yahoo.co.in>
2020-05-04 17:05:38 +0530

    git push

--- challenge3/lib/main.dart
+++ challenge3/lib/main.dart
@@ -1,84 +1,79 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+import 'widgets/card.dart';
+
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return new MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MyHomeScreen(),
+      home: new MyHomePage(),
     );
   }
 }
 
-class MyHomeScreen extends StatefulWidget {
+class MyHomePage extends StatefulWidget {
   @override
-  _MyHomeScreenState createState() => _MyHomeScreenState();
+  _MyHomePageState createState() => new _MyHomePageState();
 }
 
-class _MyHomeScreenState extends State<MyHomeScreen> {
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new FlutterLogo(colors: Colors.green, size: 25.0),
         elevation: 0.0,
-        title: FlutterLogo(
-          colors: Colors.green,
-          size: 25.0,
-        ),
         centerTitle: true,
         backgroundColor: Colors.white,
         leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () {},
-          color: Colors.grey,
-        ),
-        actions: [
+            onPressed: () {}, icon: Icon(Icons.arrow_back), color: Colors.grey),
+        actions: <Widget>[
           IconButton(
-            icon: Icon(Icons.menu),
             onPressed: () {},
+            icon: Icon(Icons.menu),
             color: Colors.grey,
           )
         ],
       ),
       body: ListView(
         shrinkWrap: true,
-        children: [
+        children: <Widget>[
           Stack(
-            children: [
+            children: <Widget>[
               Container(
                 alignment: Alignment(0.0, -0.40),
                 height: 100.0,
                 color: Colors.white,
                 child: Text(
                   'Get coaching',
-                  style: TextStyle(fontSize: 20.0),
+                  style: TextStyle(fontFamily: 'Montserrat', fontSize: 20.0),
                 ),
               ),
               Container(
                 margin: EdgeInsets.fromLTRB(25.0, 90.0, 25.0, 0.0),
                 decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(22.0),
-                  boxShadow: [
-                    BoxShadow(blurRadius: 2.0, color: Colors.grey),
-                  ],
-                ),
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20.0),
+                    boxShadow: [
+                      BoxShadow(blurRadius: 2.0, color: Colors.grey)
+                    ]),
                 child: Row(
                   crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
+                  children: <Widget>[
                     Stack(
-                      children: [
+                      children: <Widget>[
                         Container(
                           padding: EdgeInsets.fromLTRB(25.0, 25.0, 5.0, 5.0),
                           child: Text(
                             'YOU HAVE',
                             style: TextStyle(
                                 color: Colors.grey,
-                                fontWeight: FontWeight.bold),
+                                fontFamily: 'Quicksand',
+                                fontWeight: FontWeight.bold,
+                                fontSize: 14.0),
                           ),
                         ),
                         Container(
@@ -87,29 +82,26 @@ class _MyHomeScreenState extends State<MyHomeScreen> {
                             '206',
                             style: TextStyle(
                                 color: Colors.black,
+                                fontFamily: 'Quicksand',
                                 fontWeight: FontWeight.bold,
                                 fontSize: 40.0),
                           ),
-                        ),
+                        )
                       ],
                     ),
-                    SizedBox(width: 100.0),
+                    SizedBox(width: 60.0),
                     Container(
-                      height: 50.0,
+                      height: 60.0,
                       width: 125.0,
                       decoration: BoxDecoration(
                           color: Colors.greenAccent[100].withOpacity(0.5),
                           borderRadius: BorderRadius.circular(10.0)),
-                      child: MaterialButton(
-                        onPressed: () {},
-                        child: Center(
-                          child: Text(
-                            'Buy More',
+                      child: Center(
+                        child: Text('Buy more',
                             style: TextStyle(
+                                fontFamily: 'Quicksand',
                                 fontWeight: FontWeight.bold,
-                                color: Colors.green),
-                          ),
-                        ),
+                                color: Colors.green)),
                       ),
                     )
                   ],
@@ -117,41 +109,45 @@ class _MyHomeScreenState extends State<MyHomeScreen> {
               )
             ],
           ),
-          SizedBox(height: 39.0),
+          SizedBox(height: 40.0),
           Container(
-            padding: EdgeInsets.only(left: 25.0, right: 25.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'MY COACHES',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.grey,
-                      fontSize: 12.0),
-                ),
-                MaterialButton(
-                  onPressed: () {},
-                  child: Text(
-                    'VIEW PAST SESSIONS',
+              padding: EdgeInsets.only(left: 25.0, right: 25.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    'MY COACHES',
                     style: TextStyle(
+                        color: Colors.grey,
+                        fontFamily: 'Quicksand',
                         fontWeight: FontWeight.bold,
-                        color: Colors.green,
                         fontSize: 12.0),
                   ),
-                ),
-              ],
-            ),
-          ),
-          SizedBox(
-            height: 10.0,
-          ),
+                  Text(
+                    'VIEW PAST SESSIONS',
+                    style: TextStyle(
+                        color: Colors.green,
+                        fontFamily: 'Quicksand',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 12.0),
+                  )
+                ],
+              )),
+          SizedBox(height: 10.0),
           GridView.count(
             crossAxisCount: 2,
+            primary: false,
             crossAxisSpacing: 2.0,
             mainAxisSpacing: 4.0,
             shrinkWrap: true,
-            children: [],
+            children: <Widget>[
+              buildCard('Tom', 'Available', 1),
+              buildCard('Tom', 'Away', 2),
+              buildCard('Tom', 'Away', 3),
+              buildCard('Tom', 'Available', 4),
+              buildCard('Tom', 'Away', 5),
+              buildCard('Tom', 'Available', 6),
+            ],
           )
         ],
       ),
